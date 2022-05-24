4775e44862991917faf4329654cba098e766a52a
Lefdler <harold1199@gmail.com>
2022-05-23 20:41:02 -0500

    23-05-2022

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+export const About = () => (
+  <Fragment>
+    <div className="row">
+      <h3>Kimetsu No Yaiba</h3>
+      <p>
+      La adaptación a serie de anime por el estudio Ufotable fue anunciada en el número 27 de la revista Weekly Shōnen Jump el 4 de junio de 2018.48​ La serie se emitió del 6 de abril al 28 de septiembre de 2019 en Tokyo MX, GTV, GYT, BS11 y otros canales.49​50​ El anime fue dirigido por Haruo Sotozaki, con guiones del personal de Ufotable, música de Yuki Kajiura y Go Shiina, y diseño de personajes de Akira Matsushima.
+      </p>
+    </div>
+  </Fragment>
+);
